e06cf9a7e5047edcc15acbd1128a6c0a7da2a244
krejdlt22 <krejdlt22@spstrutnov.cz>
2025-05-06 10:38:14 +0200

    přidání User

---
+++ Maturita-C#/Maturita-C#/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Maturita_C_.Models;
+using System.Collections.Generic;
+
+
+namespace Maturita_C_.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Maturita-C#/Maturita-C#/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Maturita_C_.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public bool ConsentGiven { get; set; }
+    }
+}
