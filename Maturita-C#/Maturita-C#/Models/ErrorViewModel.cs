13b8db65739ccfd72fae126b8ae70d6160e9ea22
krejdlt22 <krejdlt22@spstrutnov.cz>
2025-05-06 10:32:35 +0200

    vytvoření projektu

---
+++ Maturita-C#/Maturita-C#/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Maturita_C_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
