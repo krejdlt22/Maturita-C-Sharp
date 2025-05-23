10268586f1ea03c5eca8e7e078ed41a1a676c447
Tomáš Krejdl <krejdlt22@spstrutnov.cz>
2025-05-23 21:37:24 +0200

    správné umístění Views

--- Maturita-C#/Maturita-C#/Views/Account/Home.cshtml.cs
+++ Maturita-C#/Maturita-C#/Views/Account/Home.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Maturita_C_.Views.Home
+namespace Maturita_C_.Views.Account
 {
-    public class RegisterModel : PageModel
+    public class HomeModel : PageModel
     {
         public void OnGet()
         {
